84a66dbd1a395cd98defd17954d46764ed185ee1
Patrick <tradiuz@gmail.com>
2025-09-14 13:25:26 -0500

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-HEADER_USERNAME="${HEADER_USERNAME:-"x-authentik-username"}"
-HEADER_ROLES="${HEADER_ROLES:-"x-authentik-groups"}"
-HEADER_ROLES_SEPARATOR="${HEADER_ROLES_SEPARATOR:-"|"}"
-ROLE_PLAYER="${ROLE_PLAYER:-"foundry-player"}"
-ROLE_ADMIN="${ROLE_ADMIN:-"foundry-admin"}"
+HEADER_USERNAME="${HEADER_USERNAME:-"x-auth-request-preferred-username"}"
+HEADER_ROLES="${HEADER_ROLES:-"x-auth-request-groups"}"
+ROLE_PLAYER="${ROLE_PLAYER:-"role:foundry-vtt:player"}"
+ROLE_ADMIN="${ROLE_ADMIN:-"role:foundry-vtt:admin"}"
 
 # usage: $0 patch-name file [sed-expression]
 # Utility to use sed to patch a file, verifying that it actually changed something.
@@ -46,10 +45,10 @@ patch_append() (
 )
 
 # Replace admin password check with header check.
-patch_sed admin-header-login resources/app/dist/sessions.mjs "s/testPassword(\(\w\+\)\.body\.adminPassword,\w\+,getSalt(config.passwordSalt))/(s.headers['$HEADER_ROLES'].split('$HEADER_ROLES_SEPARATOR').includes('$ROLE_ADMIN'))/"
+patch_sed admin-header-login resources/app/dist/sessions.mjs "s/testPassword(\(\w\+\)\.body\.adminPassword,\w\+,getSalt(config.passwordSalt))/(s.headers['$HEADER_ROLES'].split(',').includes('$ROLE_ADMIN'))/"
 
 # Replace user password check with header check. In addition to the player themselves admins will also be allowed to log in as any player.
-patch_sed user-header-login resources/app/dist/sessions.mjs "s/testPassword(\w\+,\(\w\+\)\.password,\w\+.passwordSalt)/((s.headers['$HEADER_USERNAME'].toLowerCase() === \1.name.toLowerCase() \&\& s.headers['$HEADER_ROLES'].split('$HEADER_ROLES_SEPARATOR').includes('$ROLE_PLAYER')) || s.headers['$HEADER_ROLES'].split('$HEADER_ROLES_SEPARATOR').includes('$ROLE_ADMIN'))/"
+patch_sed user-header-login resources/app/dist/sessions.mjs "s/testPassword(\w\+,\(\w\+\)\.password,\w\+.passwordSalt)/((s.headers['$HEADER_USERNAME'].toLowerCase() === \1.name.toLowerCase() \&\& s.headers['$HEADER_ROLES'].split(',').includes('$ROLE_PLAYER')) || s.headers['$HEADER_ROLES'].split(',').includes('$ROLE_ADMIN'))/"
 
 # Hide password fields.
 patch_append hide-password-fields resources/app/public/css/foundry2.css << END
@@ -60,10 +59,10 @@ patch_append hide-password-fields resources/app/public/css/foundry2.css << END
 END
 
 # Pass information about the user info from the headers to the client side. This is used for auto-login behavior, as well as to hide elements that aren't relevant for players.
-patch_sed track-header-info resources/app/dist/sessions.mjs "s/global\.logger\.info(\`Created client session \${\(\w\+\)\.id}\`)/(t.headerInfo = { username: s.headers['$HEADER_USERNAME'], isAdmin: s.headers['$HEADER_ROLES']?.split('$HEADER_ROLES_SEPARATOR')?.includes('$ROLE_ADMIN') ?? false }), &/"
+patch_sed track-header-info resources/app/dist/sessions.mjs "s/global\.logger\.info(\`Created client session \${\(\w\+\)\.id}\`)/(t.headerInfo = { username: s.headers['$HEADER_USERNAME'], isAdmin: s.headers['$HEADER_ROLES']?.split(',')?.includes('$ROLE_ADMIN') ?? false }), &/"
 patch_sed track-header-info resources/app/dist/server/sockets.mjs 's/\(\w\+\)\.sessionId=\(\w\+\)\.id/&,\1.headerInfo = \2.headerInfo/'
-patch_sed track-header-info resources/app/public/scripts/foundry.mjs 's/id = response\.sessionId;/& localStorage.headerInfo = JSON.stringify(response.headerInfo);/'
-patch_append track-header-info resources/app/public/scripts/foundry.mjs << END
+patch_sed track-header-info resources/app/public/scripts/foundry.js 's/id = response\.sessionId;/& localStorage.headerInfo = JSON.stringify(response.headerInfo);/'
+patch_append track-header-info resources/app/public/scripts/foundry.js << END
 	window.withHeaderInfo = (cb) => {
 		if (localStorage.headerInfo) {
 			const headerInfo = JSON.parse(localStorage.headerInfo);
@@ -73,7 +72,7 @@ patch_append track-header-info resources/app/public/scripts/foundry.mjs << END
 		}
 	};
 END
-patch_append add-non-admin-class resources/app/public/scripts/foundry.mjs << END
+patch_append add-non-admin-class resources/app/public/scripts/foundry.js << END
 	window.withHeaderInfo((headerInfo) => {
 		if (!headerInfo.isAdmin) {
 			document.body.classList.add('header-info-non-admin');
@@ -83,20 +82,11 @@ END
 
 # Auto-login users.
 # shellcheck disable=2016
-patch_append auto-login resources/app/public/scripts/foundry.mjs << END
+patch_append auto-login resources/app/public/scripts/foundry.js << END
 	window.withHeaderInfo((headerInfo) => {
 		// Check to see if the main form/placeholder exists, if it does not we're not on the right page and can just abort.
 		if (!document.querySelector('#join-game')) {
 			return;
-		}
-
-		// Add class to body to hide the form while the auto-login is in progress. This should be undone if the process
-		// fails to allow the user to select their user manually..
-		if (!headerInfo.isAdmin) {
-			document.body.classList.add('login-in-progress');
-		}
-		const showForm = () => {
-			document.body.classList.remove('login-in-progress');
 		}
 
 		const waitForTemplate = () => {
@@ -112,62 +102,61 @@ patch_append auto-login resources/app/public/scripts/foundry.mjs << END
 			// Try to find & select the matching user, aborting if none is found.
 			const item = Array.from(select?.options ?? []).find((i) => i.textContent.toLowerCase() === headerInfo.username);
 			if (!item) {
-				console.warn('Cannot find user to auto-login as.');
-				showForm();
 				return;
 			}
 			item.selected = true;
 			select.dispatchEvent(new Event('focus'));
 
-			// An admin can login as any user & go to the setup from this screen, so we'll stop after selecting their user.
-			if (headerInfo.isAdmin) {
-				return;
+			// Hide join form during auto-login.
+			if (joinButton && form && !headerInfo.isAdmin) {
+				form.style.display = 'none';
 			}
 
-			// Attempt to auto-login regular users, waiting for the notification to appear to ensure it succeeded.
+			// Attempt to auto-login, waiting for the notification to appear to ensure it succeeded.
 			let attempts = 0;
-			const notifications = document.querySelector('#notifications');
 			const tryLogin = () => {
-				if (notifications && notifications.textContent.includes('joining game')) {
+				const isLoggingIn = ui.notifications.active.map((n) => n[0]?.textContent).some((n) => n.includes('joining game'));
+				if (isLoggingIn) {
 					console.log('Auto-login succeeded.');
 					return;
 				}
 
-				joinButton.click();
+				console.log('Attempting auto-login.');
+				select.dispatchEvent(new Event('focus'));
+				if (!headerInfo.isAdmin) {
+					joinButton.click();
+				}
 
 				attempts += 1;
 				if (attempts < 10) {
 					setTimeout(tryLogin, Math.max(200, 50 * attempts));
 				} else {
-					console.warn('Auto-login failed.');
-					showForm();
+					console.log('Auto-login failed.');
+					// Giving up on auto-login, so show form again.
+					if (form) {
+						delete form.style.display;
+					}
 				}
 			};
-			console.log('Attempting auto-login.');
 			tryLogin();
 		}
 		waitForTemplate();
 	});
 END
-patch_append hide-login-form-during-autologin resources/app/public/css/foundry2.css << END
-	body.header-info-non-admin.login-in-progress #join-game-form {
-		display: none;
-	}
-END
-patch_append hide-non-admin-logout resources/app/public/css/foundry2.css << END
-	body.header-info-non-admin #settings button[data-app="logout"],
-	body.header-info-non-admin #settings section.access {
+patch_append hide-non-admin-logout resources/app/public/css/style.css << END
+	body.header-info-non-admin #settings button[data-action="logout"],
+	body.header-info-non-admin #settings h2:has(+ div:last-child > button[data-action="logout"]:only-child) {
 		display: none;
 	}
 END
 
 # Hide/change setup related items for non-admin users.
 patch_append hide-non-admin-setup resources/app/public/css/foundry2.css << END
-	body.header-info-non-admin #join-game-setup,
-	body.header-info-non-admin #setup-authentication form *:not(h2) {
+	body.header-info-non-admin #join-game .app.return-setup,
+	body.header-info-non-admin #setup-authentication *:not(h2) {
 		display: none;
 	}
-	body.header-info-non-admin #setup-authentication form > h2::after {
+	body.header-info-non-admin #setup-authentication > h2::after {
 		content: "This server is currently in setup mode and cannot be used by players.";
 		font-size: 50%;
 		display: block;
